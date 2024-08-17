e2d1edc4c20b6f19108044a411dea77bb552bc23
tehelka-gamedev <17590418+tehelka-gamedev@users.noreply.github.com>
2024-08-17 15:23:49 +0200

    fix code parsing

--- godot/main/main.gd
+++ godot/main/main.gd
@@ -56,4 +56,4 @@ func _on_object_clicked(object:GameObject) -> void:
 		object.selected = true
 		current_selected_object = object
 		
-		player_camera.attach_object(object.global_position, get_path_to(object))
+		_player_camera.attach_object(object.global_position, get_path_to(object))
