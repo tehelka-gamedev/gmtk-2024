bf0c4dcfcba8be62e8834518b5a270512a5601fc
Brendan <not@existing.abcdef>
2024-08-17 13:04:28 +0200

    Ajout du target height au billboard

--- godot/billboard/billboard.gd
+++ godot/billboard/billboard.gd
@@ -2,11 +2,18 @@ class_name Billboard
 extends StaticBody3D
 
 
+@onready var _target_height_label: Label3D = $TargetHeightLabel
 @onready var _height_label: Label3D = $HeightLabel
 
 
-func change_max_height(max_height: float) -> void:
+func set_max_height(max_height: float) -> void:
+	_height_label.text = "current height: " + height_to_str(max_height) + "m"
+
+
+func set_target_height(target_height: float) -> void:
+	_target_height_label.text = "target height: " + height_to_str(target_height) + "m"
+
+
+func height_to_str(height: float) -> String:
 	var float_precision: float = pow(10, -Const.HEIGHT_DECIMAL_PRECISION)
-	_height_label.text = str(
-			snappedf(max_height, float_precision)
-	).pad_decimals(Const.HEIGHT_DECIMAL_PRECISION)
+	return str(snappedf(height, float_precision)).pad_decimals(Const.HEIGHT_DECIMAL_PRECISION)
--- godot/main/main.gd
+++ godot/main/main.gd
@@ -1,18 +1,23 @@
 extends Node3D
 
 
+@export var target_height: float = 5.0
+
 @onready var _height_detector: HeightDetector = $HeightDetector
 @onready var _billboard: Billboard = $Billboard
 @onready var player_camera:PlayerCamera = $PlayerCamera
 
+
 func _ready() -> void:
 	@warning_ignore("return_value_discarded")
 	_height_detector.max_height_changed.connect(_on_max_height_changed)
-	
+	_billboard.set_target_height(target_height)
 	player_camera.object_clicked.connect(_on_object_clicked)
 
+
 func _on_max_height_changed(max_height: float) -> void:
-	_billboard.change_max_height(max_height)
+	_billboard.set_max_height(max_height)
+
 
 func _on_object_clicked(object:GameObject) -> void:
 	GameState.current_game_state = Enum.GameState.OBJECT_SELECTED
