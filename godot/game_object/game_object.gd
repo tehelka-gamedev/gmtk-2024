73eb056613d70cdf1fc975304263a2cd45ded166
tehelka-gamedev <17590418+tehelka-gamedev@users.noreply.github.com>
2024-08-17 23:28:03 +0200

    make game object have a list of export of mesh instances

--- godot/game_object/game_object.gd
+++ godot/game_object/game_object.gd
@@ -16,19 +16,17 @@ var selected: bool = false :
 			collision_layer = 0
 		else:
 			collision_layer = Enum.CollisionLayer.OBJECT
-			var _mesh_instance_override_material:StandardMaterial3D = _mesh_instance.get_surface_override_material(0) as StandardMaterial3D
-			assert(_mesh_instance_override_material!=null, "no mesh instance override material, something is wrong!")
-			_mesh_instance_override_material.albedo_color = Color.WHITE
+			_set_albedo_color(Color.WHITE)
 	get:
 		return selected
 var object_scale: float = 1.0:
 	set(value):
 		object_scale = value
-		_mesh_instance.scale = Vector3.ONE * object_scale
+		_set_scale(object_scale)
 		_collision_shape.scale = Vector3.ONE * object_scale
 		_collision_detector_shape.scale = Vector3.ONE * object_scale
 
-@onready var _mesh_instance: MeshInstance3D = $MeshInstance3D
+@export var _mesh_instances: Array[MeshInstance3D] = []
 @onready var _collision_detector: Area3D = $CollisionDetector
 @onready var _collision_shape: CollisionShape3D = $CollisionShape3D
 @onready var _collision_detector_shape: CollisionShape3D = $CollisionDetector/CollisionShape3D
@@ -54,12 +52,21 @@ func _unhandled_input(event: InputEvent) -> void:
 
 func _process(_delta: float) -> void:
 	if selected:
-		var _mesh_instance_override_material:StandardMaterial3D = _mesh_instance.get_surface_override_material(0) as StandardMaterial3D
 		if is_not_colliding():
-			_mesh_instance_override_material.albedo_color = valid_color
+			_set_albedo_color(valid_color)
 		else:
-			_mesh_instance_override_material.albedo_color = invalid_color
+			_set_albedo_color(invalid_color)
 
 
 func is_not_colliding() -> bool:
 	return _collision_detector.get_overlapping_bodies() == []
+
+
+func _set_albedo_color(color:Color) -> void:
+	for mesh_instance in _mesh_instances:
+		var _mesh_instance_override_material:StandardMaterial3D = mesh_instance.get_surface_override_material(0) as StandardMaterial3D
+		_mesh_instance_override_material.albedo_color = color
+
+func _set_scale(value:float) -> void:
+	for mesh_instance in _mesh_instances:
+		mesh_instance.scale = Vector3.ONE * value
