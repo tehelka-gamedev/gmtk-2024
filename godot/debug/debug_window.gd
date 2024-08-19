39ace3d48ae1e61421901de055f9d0c539786739
tehelka-gamedev <17590418+tehelka-gamedev@users.noreply.github.com>
2024-08-19 18:54:51 +0200

    add debug info and hide debug by default

--- godot/debug/debug_window.gd
+++ godot/debug/debug_window.gd
@@ -1,7 +1,7 @@
 extends MarginContainer
 
 @onready var game_state_value:Label = $VBoxContainer/HBoxContainer/GSValue
-
+@onready var mouse_pos_value:Label = $VBoxContainer/HBoxContainer2/MPValue
 
 func _ready() -> void:
 	GameState.game_state_changed.connect(_on_game_state_changed)
@@ -11,6 +11,12 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("debug_menu"):
 		visible = not visible
+		
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		mouse_pos_value.text =  str(event.position) + " / " + str(get_viewport().get_mouse_position()) + (
+			" / " + str(event.relative)
+		)
 
 
 func _on_game_state_changed(new_game_state:Enum.GameState) -> void:
